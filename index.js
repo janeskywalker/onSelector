4fabecdd6aee3fe69f5954a3fbf5cc09930cd2be
Jane Zeng <Jane@Janes-MacBook-Air.local>
2019-02-02 11:20:02 -0800

    version before event caching

--- index.js
+++ index.js
@@ -1,40 +1,11 @@
 
 
-// const handlers = {}
-const handlers = new Map()
-
-// handlers[event] = []
-// set
-// get
-// has
-// delete
-
-function addHandler(event, selector, cb) {
-    handlers.get(event).push((e) => {
-        if (recursiveMatch(e.target, selector)) {
-            cb(e)
-        }
-    })
-}
-
-function attachEventToDocument(event) {
-    document.addEventListener(event, (e) => {
-        handlers.get(event).forEach(fn => {
-            fn(e)
-        })
-    })
-}
-
 function onSelector(event, selector, cb) {
-
-    // Only executed the first time the function is called for an event
-    if(!handlers.has(event)) {
-        handlers.set(event, [])
-        attachEventToDocument(event)
-    }
-    
-    // Executed everytime the function is called
-    addHandler(event, selector, cb)
+    document.addEventListener(event, (e) => {
+        if (recursiveMatch(e.target, selector)) {
+            cb (e)
+        } 
+    })
 }
 
 // element,string -> boolean 
