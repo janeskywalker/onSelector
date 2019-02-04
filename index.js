486e2cb26e7988aafb805d9ddc2786b570bcbadb
Jane Zeng <Jane@Janes-MacBook-Air.local>
2019-02-04 14:12:00 -0800

    practice writing onSelector and recursiveMatch

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
 
 
+// function onSelector(event, selector, cb) {
+//     document.addEventListener(event, (e) => {
+//         if (recursiveMatch(e.target, selector)) {
+//             cb (e)
+//         } 
+//     })
+// }
+
+// // element,string -> boolean 
+// function recursiveMatch(el, selector) {
+//     // typeof can tell you 'string', 'number', 'boolean', 'object', 'undefined', 'symbol', 'function'
+//     if(typeof el.matches === 'function' && el.matches(selector)) {
+//         return true
+//     } else if (el.parentNode) {
+//         return recursiveMatch(el.parentNode, selector)
+//     } else {
+//         return false
+//     }
+// } 
+
+
+
+
+
 function onSelector(event, selector, cb) {
-    document.addEventListener(event, (e) => {
-        if (recursiveMatch(e.target, selector)) {
-            cb (e)
+    document.addEventListener(event, (evt) => {
+        // check the evt.target first
+        if(recursiveMatch(evt.target,selector)) {
+            cb(evt)
         } 
     })
 }
 
-// element,string -> boolean 
-function recursiveMatch(el, selector) {
-    // typeof can tell you 'string', 'number', 'boolean', 'object', 'undefined', 'symbol', 'function'
-    if(typeof el.matches === 'function' && el.matches(selector)) {
+function recursiveMatch(element, selector) {
+    if( typeof element.matches === 'function' && element.matches(selector)) {
         return true
-    } else if (el.parentNode) {
-        return recursiveMatch(el.parentNode, selector)
+        // check parentNode is not null
+    } else if(element.parentNode) {
+        return recursiveMatch(element.parentNode, selector)
     } else {
         return false
     }
-} 
+
+    
+}
