e5a25fd38bdd96fda60cc8a2c0c678ccad8f792f
Jane Zeng <Jane@Janes-MacBook-Air.local>
2019-02-07 10:55:42 -0800

    add webpack

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
 
 const handler = new Map
 
-// function onSelector(event, selector, cb) {
-//     document.addEventListener(event, (e) => {
-//         if (recursiveMatch(e.target, selector)) {
-//             cb (e)
-//         } 
-//     })
-// }
+function attachEventToDocument(event) {
+    document.addEventListener(event, (evt) => {
+        handler.get(event).forEach((fn) => {
+            fn(evt)
+        })
+        
+    }) 
+}
 
-
-
-function onSelector(event, selector, cb) {
-    
-    if(!handler.has(event)) {
-        handler.set(event, [])
-
-        document.addEventListener(event, (evt) => {
-            //debugger
-            //if(recursiveMatch(evt.target, selector)) {
-            handler.get(event).forEach((fn) => {
-                fn(evt)
-            })
-            //}
-        }) 
-    }
-
+function addHandler(event, selector, cb) {
     handler.get(event).push((evt) => {
-         
         if(recursiveMatch(evt.target, selector)) {
             cb(evt)
         }
@@ -36,59 +20,19 @@ function onSelector(event, selector, cb) {
 
 
 
+function onSelector(event, selector, cb) {
+    
+    if(!handler.has(event)) {
+        handler.set(event, [])
+        attachEventToDocument(event)
+    }
 
-// function onSelector(event, selector, cb) {
-
-//     console.log('cb:', cb)
-//     // add the event for the fist time, set the key 
-//     // addEventListener to doc, run all fns 
-//     if(!handler.has(event)) {
-//         handler.set(event, [])
-
-//         // console.log('adding event:', event)
-//         // console.log('handler:', handler)
-        
-//         // triggered when event fired
-//         // fn is the new cb that calls old cb
-//         document.addEventListener(event, (evt) => {
-//             debugger
-
-//             handler.get(event).forEach((fn) => {
-//                 //console.log('fn:', fn)
-//                 fn(evt)
-//             })
-//         }) 
-
-//     }
-
-//     // if event already exist, push a new cb that 
-//     // calls the old cb
-//     handler.get(event).push((evt) => {
-
-//         //console.log('handler:', handler)
-//         // console.log('added event:', event)
-
-//         // console.log('evt.target:', evt.target)
-//         // console.log('selector:', selector)
-         
-//         if(recursiveMatch(evt.target, selector)) {
-//             console.log('cb:', cb)
-//             cb(evt)
-//         }
-//     })
-
-//     //console.log('handler:', handler)
-// }
+    addHandler(event, selector, cb)
+}
 
 
 
 
-
-
-
-
-
-// element,string -> boolean 
 function recursiveMatch(el, selector) {
     // typeof can tell you 'string', 'number', 'boolean', 'object', 'undefined', 
     //'symbol', 'function'
@@ -102,25 +46,3 @@ function recursiveMatch(el, selector) {
 } 
 
 
-
-
-
-// function onSelector(event, selector, cb) {
-//     document.addEventListener(event, (evt) => {
-//         // check the evt.target first
-//         if(recursiveMatch(evt.target,selector)) {
-//             cb(evt)
-//         } 
-//     })
-// }
-
-// function recursiveMatch(element, selector) {
-//     if( typeof element.matches === 'function' && element.matches(selector)) {
-//         return true
-//         // check parentNode is not null
-//     } else if(element.parentNode) {
-//         return recursiveMatch(element.parentNode, selector)
-//     } else {
-//         return false
-//     }
-// }
