8c918f332275c97ce433373538a6dda41349a2cc
Jane Skywalker <janeskywalker@Kevins-MacBook-Pro.local>
2019-02-08 10:31:24 -0800

    install prettier and eslint

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const handler = new Map();
+
+function attachEventToDocument(event) {
+    document.addEventListener(event, evt => {
+        handler.get(event).forEach(fn => {
+            fn(evt);
+        });
+    });
+}
+
+function addHandler(event, selector, cb) {
+    handler.get(event).push(evt => {
+        if (recursiveMatch(evt.target, selector)) {
+            cb(evt);
+        }
+    });
+}
+
+export function onSelector(event, selector, cb) {
+    if (!handler.has(event)) {
+        handler.set(event, []);
+        attachEventToDocument(event);
+    }
+
+    addHandler(event, selector, cb);
+}
+
+function recursiveMatch(el, selector) {
+    // typeof can tell you 'string', 'number', 'boolean', 'object', 'undefined',
+    //'symbol', 'function'
+    if (typeof el.matches === "function" && el.matches(selector)) {
+        return true;
+    } else if (el.parentNode) {
+        return recursiveMatch(el.parentNode, selector);
+    } else {
+        return false;
+    }
+}
+
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+module.exports = {
+    mode: 'development',
+    // mode: 'production',
+    entry: './src/index.js',
+    output: {
+        // __dirname is a variable that holds path to this file.
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    }
+}
