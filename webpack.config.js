161c0fa91301761aaf8961cc539f2dafe3e3e326
Jane Skywalker <janeskywalker@Kevins-MacBook-Pro.local>
2019-02-09 11:52:17 -0800

    1.0.4

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,17 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     // mode: 'production',
+    // tell webpack this whole thing is a module, not an app
     entry: './src/index.js',
     output: {
         // __dirname is a variable that holds path to this file.
         path: path.resolve(__dirname, 'dist'),
         filename: 'onselector.js',
+        // expose the library
         library: 'onselector',
+        // unuversial module definition, flexible module that fits in 
+        // various enviroment 
         libraryTarget: 'umd',
     }
 }
+
