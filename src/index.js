4632ddc86f25d1bc90e79b9e5eb0df35d900647f
Jane Skywalker <janeskywalker@Kevins-MacBook-Pro.local>
2019-02-09 10:17:42 -0800

    add gitignore file

--- src/index.js
+++ src/index.js
@@ -1,39 +1,38 @@
-const handler = new Map();
+const handler = new Map()
 
 function attachEventToDocument(event) {
     document.addEventListener(event, evt => {
         handler.get(event).forEach(fn => {
-            fn(evt);
-        });
-    });
+            fn(evt)
+        })
+    })
 }
 
 function addHandler(event, selector, cb) {
     handler.get(event).push(evt => {
         if (recursiveMatch(evt.target, selector)) {
-            cb(evt);
+            cb(evt)
         }
-    });
+    })
 }
 
 export function onSelector(event, selector, cb) {
     if (!handler.has(event)) {
-        handler.set(event, []);
-        attachEventToDocument(event);
+        handler.set(event, [])
+        attachEventToDocument(event)
     }
 
-    addHandler(event, selector, cb);
+    addHandler(event, selector, cb)
 }
 
 function recursiveMatch(el, selector) {
     // typeof can tell you 'string', 'number', 'boolean', 'object', 'undefined',
     //'symbol', 'function'
-    if (typeof el.matches === "function" && el.matches(selector)) {
-        return true;
+    if (typeof el.matches === 'function' && el.matches(selector)) {
+        return true
     } else if (el.parentNode) {
-        return recursiveMatch(el.parentNode, selector);
+        return recursiveMatch(el.parentNode, selector)
     } else {
-        return false;
+        return false
     }
 }
-
